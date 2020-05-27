3ef64a52b453feaf827ad51270c67457c2d1cebd
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-05-27 22:06:45 +0000

    Added Latvian translation.

--- Cryptocurrency.Blockchain/Transaction.cs
+++ Cryptocurrency.Blockchain/Transaction.cs
@@ -27,3 +27,43 @@ namespace Cryptocurrency.Blockchain
             {
                 this.hash = value;
             }
+        }
+
+        [DataMember(Order = 2)]
+        public string Signature { get; set; }
+
+        [DataMember(Order = 3)]
+        public List<TransactionInput> Inputs { get; set; } = new List<TransactionInput>();
+
+        [DataMember(Order = 4)]
+        public List<TransactionOutput> Outputs { get; set; } = new List<TransactionOutput>();
+
+        public void Sign(PrivateKey privateKey)
+        {
+            this.Signature = Convert.ToBase64String(Sha256EcdsaSignerService.SignData(privateKey, this.Hash));
+        }
+
+        public bool VerifySignature(PublicKey publicKey)
+        {
+            return Sha256EcdsaSignerService.VerifySignature(publicKey, this.Signature, this.Hash);
+        }
+
+        internal string ComputeHash()
+        {
+            List<byte> allBytes = new List<byte>();
+
+            foreach (TransactionInput txIn in this.Inputs)
+            {
+                allBytes.AddRange(ChainParams.Encoder.GetBytes(txIn.PreviousTransactionHash));
+            }
+
+            foreach (TransactionOutput txOut in this.Outputs)
+            {
+                allBytes.AddRange((byte[])txOut.Amount);
+                allBytes.AddRange(ChainParams.Encoder.GetBytes(Sha256Hash.Hash(txOut.To)));
+            }
+
+            return Sha256Hash.Hash(allBytes.ToArray());
+        }
+    }
+}
