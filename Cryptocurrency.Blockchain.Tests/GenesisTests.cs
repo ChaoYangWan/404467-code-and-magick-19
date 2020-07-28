9713a53ce6179cf5a8c5b2cbb516d24dae66658b
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-07-28 09:39:14 +0000

    cleaned up whitespace

---
+++ Cryptocurrency.Blockchain.Tests/GenesisTests.cs
@@ -0,0 +1,20 @@
+
+﻿using Cryptocurrency.Database.Factories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptocurrency.Blockchain.Tests
+{
+    [TestClass]
+    public class GenesisTests
+    {
+        [TestMethod]
+        public void GenesisBlock()
+        {
+            Block block = BlockFactory.GenesisBlock();
+            Assert.IsNotNull(block);
+        }
+    }
+}
