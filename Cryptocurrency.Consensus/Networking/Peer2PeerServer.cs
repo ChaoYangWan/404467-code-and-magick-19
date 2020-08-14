60704cd465cbd27b3f84b3ab6b80676ce54c1bc3
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-08-14 22:21:47 +0000

    CDN

--- Cryptocurrency.Consensus/Networking/Peer2PeerServer.cs
+++ Cryptocurrency.Consensus/Networking/Peer2PeerServer.cs
@@ -9,3 +9,50 @@ using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cryptocurrency.Consensus.Networking
+{
+    public class Peer2PeerServer
+    {
+        internal TcpListener Server { get; private set; }
+        internal List<Peer> Peers { get; private set; }
+
+        private CancellationTokenSource cancellationToken;
+
+        public Peer2PeerServer()
+        {
+            this.Server = new TcpListener(IPAddress.Any, Keys.DefaultPort);
+            this.Peers = new List<Peer>();
+        }
+
+        public void Start()
+        {
+            this.Server.Start();
+            this.ServerLoopFactory();
+        }
+
+        private void ServerLoopFactory()
+        {
+            this.cancellationToken = new CancellationTokenSource();
+
+            Task.Run(() =>
+            {
+                while (!this.cancellationToken.IsCancellationRequested)
+                {
+                    Peer peer = new Peer(this.Server.AcceptTcpClient());
+                    this.Peers.Add(peer);
+                }
+            });            
+        }
+
+
+        private void PeerDiscoveryHandler()
+        {
+
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Cryptocurrency.Consensus/Networking/PeerMessage.cs
@@ -0,0 +1,9 @@
+
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptocurrency.Consensus.Networking
