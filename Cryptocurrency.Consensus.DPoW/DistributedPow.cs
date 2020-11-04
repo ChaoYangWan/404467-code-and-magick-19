7edbdf177bf67a729d7bb5b4c24c27af4c853102
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-11-04 14:35:56 +0000

    template updates

--- Cryptocurrency.Consensus.DPoW/DistributedPow.cs
+++ Cryptocurrency.Consensus.DPoW/DistributedPow.cs
@@ -7,4 +7,4 @@ namespace Cryptocurrency.Consensus.DPoW
         /*
 
          The concept is each node in the network accepts ownership of a parcel (like pass the parcel) one at a time and they have to unravel one layer
-         In order to unravel a layer, it must do a quick and easy PoW puzzle. Once it solves t
+         In order to unravel a layer, it must do a quick and easy PoW puzzle. Once it solves the puzzle, it broadcasts the new parcel to the network for 
