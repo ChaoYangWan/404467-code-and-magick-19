d6eebe206d497bc4d3a40fe19d59008489ca97e1
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-11-04 20:02:02 +0000

    Fix JSCS

--- Cryptocurrency.Consensus.DPoW/DistributedPow.cs
+++ Cryptocurrency.Consensus.DPoW/DistributedPow.cs
@@ -9,4 +9,7 @@ namespace Cryptocurrency.Consensus.DPoW
          The concept is each node in the network accepts ownership of a parcel (like pass the parcel) one at a time and they have to unravel one layer
          In order to unravel a layer, it must do a quick and easy PoW puzzle. Once it solves the puzzle, it broadcasts the new parcel to the network for the next node
          randomly, a node will accept a parcel and unwrap it completely. Thus mining a block.
-         The node that completely unwraps a parcel determines how many layers the next parcel
+         The node that completely unwraps a parcel determines how many layers the next parcel has as part of the "mining difficulty"
+
+         Problems:
+  
