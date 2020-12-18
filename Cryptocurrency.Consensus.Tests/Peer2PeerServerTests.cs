0ae75aacb70da47d3775b2de884a32f01f9b572a
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2020-12-18 21:27:03 +0000

    Refactor config retrieve

--- Cryptocurrency.Consensus.Tests/Peer2PeerServerTests.cs
+++ Cryptocurrency.Consensus.Tests/Peer2PeerServerTests.cs
@@ -3,4 +3,7 @@ using Cryptocurrency.Consensus.Networking;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 
-namespace Cry
+namespace Cryptocurrency.Consensus.Tests
+{
+    [TestClass]
+    public class 
