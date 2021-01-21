75c42846b4dcf6d3700a8b3a08ee4cef8a8459bb
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2021-01-21 22:42:37 +0000

    Add missing ChangeLog entries.

--- Cryptocurrency.Core/Services/TransactionValidatorService.cs
+++ Cryptocurrency.Core/Services/TransactionValidatorService.cs
@@ -33,4 +33,6 @@ namespace Cryptocurrency.Services
                 totalInput += lastTxInOutput.Amount;
             }
 
-      
+            if (isCoinbase)
+            {
+                if (transaction.
