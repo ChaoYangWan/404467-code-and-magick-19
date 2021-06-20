1df2b89d91eab575cf2c7b2b82637faafbcf3041
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2021-06-20 20:09:40 +0000

    Update to new version.

--- Cryptocurrency.Cryptography/Factories/SignerFactory.cs
+++ Cryptocurrency.Cryptography/Factories/SignerFactory.cs
@@ -6,4 +6,7 @@ using Org.BouncyCastle.Math;
 using Org.BouncyCastle.Math.EC;
 using Org.BouncyCastle.Security;
 using System;
-usin
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cryptocurrency.Crypt
