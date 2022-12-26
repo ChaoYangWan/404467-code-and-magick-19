5b43eec3209031a85105ad6fa3a73b59bcf05576
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2022-12-26 16:23:26 +0000

    add TFT Display

--- Cryptocurrency.Database/Factories/BlockFactory.cs
+++ Cryptocurrency.Database/Factories/BlockFactory.cs
@@ -2,4 +2,8 @@
 using Cryptocurrency.Cryptography;
 using System;
 using System.Collections.Generic;
-using System.Globalizatio
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace Cryptocurrency.
