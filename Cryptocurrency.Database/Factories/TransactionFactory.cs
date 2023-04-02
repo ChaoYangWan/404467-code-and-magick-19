f3b8cf5986a26369773ff5cf7d803cf25ce54d7a
ChaoYangWan <92674886+ChaoYangWan@users.noreply.github.com>
2023-04-02 11:23:44 +0000

    Forward port zendframework/zf2#3620

--- Cryptocurrency.Database/Factories/TransactionFactory.cs
+++ Cryptocurrency.Database/Factories/TransactionFactory.cs
@@ -9,3 +9,18 @@ namespace Cryptocurrency.Database.Factories
 {
     public static class TransactionFactory
     {
+        public static Transaction TransactionFromTxInput (TransactionInput txInput)
+        {
+            using (BlockchainDatabase db = BlockchainDatabase.Create())
+            {
+                string previousTx = txInput.PreviousTransactionHash;
+                Block blockWithTransaction = db.Blocks.FindOne(y => y.Transactions.Exists(z => z.Hash == previousTx));
+
+                if (blockWithTransaction == null)
+                    return null;
+
+                return blockWithTransaction.Transactions.FirstOrDefault(y => y.Hash == previousTx);
+            }
+        }
+    }
+}
